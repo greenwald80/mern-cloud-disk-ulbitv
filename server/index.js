06539f2ed1bfe6eba5cb24e22d17ab6287a131a2
Alexander <greenwald80@gmail.com>
2021-04-14 11:37:19 +0300

    DB projecting and connecting to mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
-console.log('MERN');
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+const PORT = config.get("serverPort");
+const URL = config.get("dbUrl");
+
+const start = async () => {
+  try {
+    mongoose.connect(
+      URL,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err) => {
+        if (err) throw new Error();
+        console.log("Connected to MongoDB");
+      }
+    );
+    app.listen(PORT, () => {
+      console.log("Server started on port ", PORT);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
