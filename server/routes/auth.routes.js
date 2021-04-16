c511c3fa732ef06e2c3754c94dab1ede4cb504fa
Alexander <greenwald80@gmail.com>
2021-04-16 14:16:39 +0300

    added JWT for authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const router = new Router();
 
@@ -30,7 +32,7 @@ router.post(
           .status(400)
           .json({ message: `User with email ${email} already exists` });
       }
-      const hashPassword = await bcrypt.hash(password, 15);
+      const hashPassword = await bcrypt.hash(password, 8);
       const user = new User({ email, password: hashPassword });
       await user.save();
       return res.json({ message: "User was created successfully " });
@@ -40,5 +42,38 @@ router.post(
     }
   }
 );
+
+// http://localhost:3000/api/auth/login =>
+// POST + Body + raw + JSON =>
+// {"email":"t@mail.com","password":"pass"}
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    const isPassValid = bcrypt.compareSync(password, user.password);
+    if (!isPassValid) {
+      res.status(400).json({ message: "Incorrect password" });
+    }
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
+      expiresIn: "1h",
+    });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Server error" });
+  }
+});
 
 module.exports = router;
